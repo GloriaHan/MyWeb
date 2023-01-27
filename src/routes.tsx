96c7d81e74e79c51eb21ee702784ece07a252265
Gloria <hanning_hit@hotmail.com>
2023-01-27 13:01:15 +1100

    add styled components style

---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import HomePage from "pages/HomePage/homepage";
+
+function Routings() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Routings;
