9747045d86d0c022fdf785d9d2c5ddf71c43aca5
Gloria <hanning_hit@hotmail.com>
2023-02-05 15:59:19 +1100

    update resume

--- src/pages/HomePage/homepage.tsx
+++ src/pages/HomePage/homepage.tsx
@@ -11,7 +11,9 @@ const HomePage = (): JSX.Element => {
         <h5>Hey I'm</h5>
         <h1>Gloria Han</h1>
         <h5 id="role">Front End Developer</h5>
+        <a href='https://file-store-gloria.s3.ap-southeast-2.amazonaws.com/resume-Ning+Han.pdf'>
         <button>Download Resume</button>
+        </a>
       </Header>
       <Main>
         <CubeContainer>
